ebcb7411ccadcd991e7f9b7dcbf5a5a2fe87dc4c
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-11-29 22:24:54 +0200

    Add footer partial and update main layout structure
    Introduced a new footer partial with company contact details and integrated it into the main layout. Updated layout structure to use <main> and <footer> elements, improving semantic HTML and maintainability. Also removed commented-out footer code from the welcome view. Updated public icon assets.

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @include('partials.head')
-    </head>
-    <body class="flex min-h-screen flex-col items-center justify-center p-6 lg:p-8">
-        {{ $slot }}
-        @fluxScripts
-    </body>
+<head>
+    @include('partials.head')
+</head>
+<body class="flex min-h-screen flex-col">
+<main class="flex flex-1 items-center justify-center p-6 lg:p-8">
+    {{ $slot }}
+</main>
+<footer class="text-center p-6 lg:p-8">
+    @include('partials.footer')
+</footer>
+@fluxScripts
+</body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<flux:text>&copy; {{ now()->year }} {{__('S&L Media SIA')}}</flux:text>
+<flux:text class="mt-1"
+>{{__('Enerģētikas iela 2-5, Mālpils, Siguldas novads, Latvija, LV-2152')}}</flux:text>
+<flux:text class="mt-1">{{__('Reg. number:')}} 40203531461</flux:text>
+<flux:text class="mt-1">{{__('E-mail:')}}
+    <flux:link href="mailto:linards@slmedia.lv">linards@slmedia.lv</flux:link>
+</flux:text>
+<flux:text class="mt-1">{{__('Phone:')}}
+    <flux:link href="tel:+37128324128">+371 2 83 24 128</flux:link>
+</flux:text>
